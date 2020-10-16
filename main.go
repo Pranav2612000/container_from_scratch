b38906961e4932a9bdd9b409bd0a91d7e822cb00
Pranav Joglekar <pranav2000joglekar@gmail.com>
2020-10-16 11:59:34 +0530

    feat: each process gets PID 1

--- main.go
+++ main.go
@@ -13,21 +13,32 @@ func main() {
   switch os.Args[1] {
   case "run":
     run()
+  case "child":
+    child()
   default:
     panic("Syntax Error!\ngo run main.go run <cmd> <args>")
   }
 }
 
 func run() {
+  cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+  cmd.Stdin = os.Stdin
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+  cmd.SysProcAttr = &syscall.SysProcAttr {
+    Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+  }
+
+  must(cmd.Run())
+}
+
+func child() {
   fmt.Printf("running %v as PID %d\n ", os.Args[2:], os.Getpid())
   cmd := exec.Command(os.Args[2], os.Args[3:]...)
   cmd.Stdin = os.Stdin
   cmd.Stdout = os.Stdout
   cmd.Stderr = os.Stderr
-  cmd.SysProcAttr = &syscall.SysProcAttr {
-    Cloneflags: syscall.CLONE_NEWUTS,
-  }
-  
+
   must(cmd.Run())
 }
 
